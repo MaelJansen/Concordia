52d7369b3eaf76b931c39651672e9e4c61cf5deb
ztanji <ztanji@2a02-8440-6241-a529-665d-86ff-fec7-179e.rev.sfr.net>
2023-09-01 11:18:03 +0200

    [Test Unitaire] Specialist, Prefect, Colonist

--- Code/test/PersonalityTest.py
+++ Code/test/PersonalityTest.py
@@ -1,3 +1,9 @@
+import unittest
+import typing
+from Code.src.Personality import Specialist
+from Code.src.Player import *
+from Code.src.Map import *
+
 class PersonalityTest:
     pass
 
@@ -5,7 +11,24 @@ class ArchitectTest:
     pass
 
 class Colonist_Test:
-    pass
+    
+ def test_colonist(self):
+    player: Player = Player(0, 0, (12, 14, 15))
+    city: City = City("I", (12, 14, 15))
+
+    city.add_house(player)
+
+    player.my_store_house = ({"wheat": 1, "tools": 1})
+
+    colonist_count: int = 2 
+    for _ in range(colonist_count):
+        colonist: Colonist = Colonist("Colonist", "Colonist", None)
+        player.add_card(colonist)
+
+    colonist.personality_action()
+
+    expected_sesterces: int = 5 + colonist_count  
+    self.assertEqual(player.get_sesterces(), expected_sesterces)
 
 class ConcordiaTest:
     pass
@@ -20,7 +43,29 @@ class MercatorTest:
     pass
 
 class PrefectTest:
-    pass
+    
+    
+    def test_perfect(self): 
+        player: Player = Player(0, 0, (12, 14, 15))
+        province: Province = Province("I", (12, 14, 15))
+
+        player.add_province_token(province)
+
+        choice: str = "bonuses_and_products"  
+
+        player.return_province_token(choice)
+
+        if choice == "bonuses_and_products":
+            expected_bonuses = {"wood": 2, "clay": 1, "wheat": 3}  
+            expected_products = {"wood": 4, "clay": 2, "wheat": 5}  
+            self.assertEqual(player.get_production_bonuses(), expected_bonuses)
+            self.assertEqual(player.get_province_products(), expected_products)
+        elif choice == "coins":
+            for province in player.get_provinces():
+                self.assertTrue(province.is_active())
+            expected_coins: int = 10  
+            self.assertEqual(player.get_sesterces(), expected_coins)
+
 
 class PrefectusMagnusTest:
     pass
@@ -29,7 +74,35 @@ class SenatorTest:
     pass
 
 class SpecialistTest:
-    pass
+    
+    def test_specialist(self):
+        specialist: Specialist = Specialist("Mason","Specialist",null)
+        
+        player: Player = Player(0,0, (12,14,15)) 
+        player.add_card(specialist)
+        
+        city: City = City("I", (12,14,15))
+        city.add_house(player)
+        
+        specialist.personality_action()
+        self.assertEqual(player.my_store_house[0].type, "brick")
+        
+        
+    def test_specialist2(self):
+        specialist: Specialist = Specialist("Mason","Specialist",null)
+        
+        player: Player = Player(0,0, (12,14,15)) 
+        player.add_card(specialist)
+        
+        city: City = City("I", (12,14,15))
+        city.add_house(player)
+        
+        specialist.personality_action()
+        self.assertEqual(player.my_store_house[0].type, "brick")
+        
+        specialist.personality_action()
+        self.assertEqual(player.my_store_house[1].type, null)
+        
 
 class TribuneTest:
     pass
