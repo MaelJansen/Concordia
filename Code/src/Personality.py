5d487bd7813446d3bbb5095216a5998c8666a8e1
ztanji <ztanji@2a02-8440-6241-a529-665d-86ff-fec7-179e.rev.sfr.net>
2023-09-04 15:27:36 +0200

    Personality Colonist done

--- Code/src/Personality.py
+++ Code/src/Personality.py
@@ -8,6 +8,7 @@ from Player import Player
 from Map import Map
 import typing
 from Piece import Ressource
+from Piece import Colonist
 
 class Personality(ABC):
     def __init__(self):
@@ -34,16 +35,14 @@ class Colonist_(Personality):
         card_action: str = 'You may place a colonist on a city tile that does not have a colonist.'
     
     @abstractmethod
-    def personality_action(player: Player):
+    def personality_action(player: Player, root: tk.Tk, map: Map):
         windows = tk.Toplevel(root)
         windows.title("Configuration de l'action")
         windows.resizable(False, False)
 
-        # Dimensions de la fenêtre
         windows_width = 300
         windows_height = 110
 
-        # Récupération des dimensions de l'écran
         dimensions = get_monitors()[0]
 
         x_coordinate = int((dimensions.width - windows_width) // 2)
@@ -60,13 +59,43 @@ class Colonist_(Personality):
             colon_count = player.my_colonists.count
             wheat_needed = colon_count 
             tool_needed = colon_count  
+            
+            if player.my_store_house.my_ressources[0].count < wheat_needed or player.my_store_house.my_ressources[1].count < tool_needed:
+                action_result_label.config(text="Vous n'avez pas assez de ressources pour placer des colons.")
+                return
+            
+            player.my_store_house.my_ressources[0].count -= wheat_needed
+            player.my_store_house.my_ressources[1].count -= tool_needed
+            player.my_colonists.count += colon_count
 
-            if wheat_needed <= player_wheat_count and tool_needed <= player_tool_count:
-                player_wheat_count -= wheat_needed
-                player_tool_count -= tool_needed
+            city_tile_window = tk.Toplevel(root)
+            city_tile_window.title("Choix de la tuile")
+            city_tile_window.resizable(False, False)
+            
+            city_tile_window_width = 300
+            city_tile_window_height = 110
+            
+            dimensions = get_monitors()[0]
+            
+            x_coordinate = int((dimensions.width - city_tile_window_width) // 2)
+            y_coordinate = int((dimensions.height - city_tile_window_height) // 2)
+            
+            city_tile_window.geometry(f"{city_tile_window_width}x{city_tile_window_height}+{x_coordinate}+{y_coordinate}")
+            
+            city_tile_label = tk.Label(city_tile_window, text="Choisissez une ville:")
+            city_tile_label.pack()
+            
+            city_tile_box = ttk.Combobox(city_tile_window, values=map.city_tiles)
+            city_tile_box.pack()
+                  
+            new_colon: Colonist = Colonist()
+            new_colon.move(map.city_tiles[city_tile_box.current()])
+            
 
         def collect_sesterces():
             action_result_label.config(text="Vous avez choisi de récupérer des sesterces.")
+            
+            player.money += player.my_colonists.count + 5 
 
         place_colons_button = tk.Button(windows, text="Placer des colons", command=place_colons)
         place_colons_button.pack()
