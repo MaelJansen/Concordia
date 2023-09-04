e7defb444671b7685b4f11db804e508485a5369c
ztanji <ztanji@2a02-8440-6241-a529-665d-86ff-fec7-179e.rev.sfr.net>
2023-09-04 15:16:05 +0200

    Start Colonist card

--- Code/src/Personality.py
+++ Code/src/Personality.py
@@ -1,4 +1,12 @@
 from abc import ABC, abstractmethod
+from ast import List, Tuple
+import tkinter as tk
+from tkinter import ttk
+from PIL import ImageTk, Image
+from screeninfo import get_monitors
+from Player import Player
+from Map import Map
+import typing
 from Piece import Ressource
 
 class Personality(ABC):
@@ -19,12 +27,58 @@ class Architect(Personality):
         pass
 
 class Colonist_(Personality):
+    
     def __init__(self):
         super().__init__()
-
+        name: str = 'Colonist'
+        card_action: str = 'You may place a colonist on a city tile that does not have a colonist.'
+    
     @abstractmethod
-    def personality_action():
-        pass
+    def personality_action(player: Player):
+        windows = tk.Toplevel(root)
+        windows.title("Configuration de l'action")
+        windows.resizable(False, False)
+
+        # Dimensions de la fenêtre
+        windows_width = 300
+        windows_height = 110
+
+        # Récupération des dimensions de l'écran
+        dimensions = get_monitors()[0]
+
+        x_coordinate = int((dimensions.width - windows_width) // 2)
+        y_coordinate = int((dimensions.height - windows_height) // 2)
+
+        windows.geometry(f"{windows_width}x{windows_height}+{x_coordinate}+{y_coordinate}")
+
+        action_label = tk.Label(windows, text="Choisissez une action:")
+        action_label.pack()
+
+        def place_colons():
+            action_result_label.config(text="Vous avez choisi de placer des colons.")
+            
+            colon_count = player.my_colonists.count
+            wheat_needed = colon_count 
+            tool_needed = colon_count  
+
+            if wheat_needed <= player_wheat_count and tool_needed <= player_tool_count:
+                player_wheat_count -= wheat_needed
+                player_tool_count -= tool_needed
+
+        def collect_sesterces():
+            action_result_label.config(text="Vous avez choisi de récupérer des sesterces.")
+
+        place_colons_button = tk.Button(windows, text="Placer des colons", command=place_colons)
+        place_colons_button.pack()
+
+        collect_sesterces_button = tk.Button(windows, text="Récupérer des sesterces", command=collect_sesterces)
+        collect_sesterces_button.pack()
+
+        action_result_label = tk.Label(windows, text="")
+        action_result_label.pack()
+
+        windows.mainloop()
+            
 
 class Concordia(Personality):
     def __init__(self):
