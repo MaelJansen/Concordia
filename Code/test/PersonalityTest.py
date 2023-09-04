c72ea7a7402df8ca8963f88b9b2e58e754446705
Vincent <vincent.guyot@u-bordeaux.fr>
2023-09-04 10:55:16 +0200

    refactor

--- Code/test/PersonalityTest.py
+++ Code/test/PersonalityTest.py
@@ -9,10 +9,8 @@ from Code.src.Map import *
 class PersonalityTest:
     pass
 
-
 class ArchitectTest:
     pass
-
 
 class Colonist_Test:
     
@@ -34,54 +32,41 @@ class Colonist_Test:
     expected_sesterces: int = 5 + colonist_count  
     self.assertEqual(player.get_sesterces(), expected_sesterces)
 
-
 class ConcordiaTest:
     pass
 
-
 class ConsulTest:
-    # Purchase 1 new personnality card without additionnal costs and put it in your hand
     def test_consul(self):
         consul: Consul = Consul()
+
         player_test: Player = Player()
+        
         nb_cards_before: int = len(player_test.hand)
         card: Card = Card()
+
         consul.personality_action(player_test, card)
         nb_cards_after = len(player_test.hand)
-        self.assertEqual(nb_cards_after, nb_cards_before + 1)
-        return True
 
+        self.assertEqual(nb_cards_after, nb_cards_before + 1)
 
 class DiplomatTest:
-    # Use an opponent's top face up personality card in his discard pile
     def test_diplomat(self):
-        # Créez une instance de la classe Diplomat
         diplomat: Diplomat = Diplomat()
 
-        # Créez deux instances de la classe Joueur
         player1: Player = Player()
         player2: Player = Player()
 
-        # Créez une carte de test et ajoutez-la à la défausse du joueur2
         test_card: Card = Card()
         player2.discard_pile.append(test_card)
 
-        # Appelez la méthode qui permet à Diplomat d'utiliser la carte du dessus de la défausse d'un autre joueur
         diplomat.personality_action(player2)
 
-        # Vérifiez que la carte a été utilisée par Diplomat (par exemple, en vérifiant l'état de Diplomat après l'utilisation)
         self.assertIn(diplomat, player1.discard_pile)
-
-        return True
-
 
 class MercatorTest:
     pass
 
-
 class PrefectTest:
-    
-    
     def test_perfect(self): 
         player: Player = Player(0, 0, (12, 14, 15))
         province: Province = Province("I", (12, 14, 15))
@@ -103,44 +88,29 @@ class PrefectTest:
             expected_coins: int = 10  
             self.assertEqual(player.get_sesterces(), expected_coins)
 
-
-
 class PrefectusMagnusTest:
     pass
 
-
 class SenatorTest:
-    # Purchase up to 2 new personality and put them into your hand
     def test_senator(self):
-        # Créez une instance de la classe Senator
         senator: Senator = Senator()
 
-        # Créez une instance de la classe Joueur
         player: Player = Player()
 
-        # Créez deux cartes de personnalité de test
         card1: Card = Card()
         card2: Card = Card()
 
-        # Créez le marché
         marketplace: MarketPlace = MarketPlace()
 
-        # Appelez la méthode d'achat de cartes de personnalité de Senator
         senator.personality_action(player, card1, card2)
 
-        # Vérifiez que les cartes ont été ajoutées à la main du joueur
         self.assertIn(card1, player.discard_pile)
         self.assertIn(card2, player.discard_pile)
 
-        # Vérifiez que les cartes ont été retirées des cartes de personnalité disponibles dans la partie
         self.assertNotIn(card1, marketplace.display_area)
         self.assertNotIn(card2, marketplace.display_area)
 
-        return True
-
-
 class SpecialistTest:
-    
     def test_specialist(self):
         specialist: Specialist = Specialist("Mason","Specialist",null)
         
@@ -168,8 +138,6 @@ class SpecialistTest:
         
         specialist.personality_action()
         self.assertEqual(player.my_store_house[1].type, null)
-        
-
 
 class TribuneTest:
     pass
