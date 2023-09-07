4b70edd490e131c35e481b4ac38556b964e902ea
juledupont <jules.dupont.1@etu.u-bordeaux.fr>
2023-09-07 16:17:48 +0200

    wip setup cards

--- Code/src/Players.py
+++ Code/src/Players.py
@@ -23,6 +23,8 @@ class Player:
     -------
 
     """
+    #static
+    MAX_HOUSES = 0
     def __init__(self):
         self.n_point = 0
         self.money = 0
@@ -30,8 +32,8 @@ class Player:
         self.my_store_house = StoreHouse()
         self.my_colonist = []
         self.discard_pile = []
-        self.hand = []
-        self.my_houses = []
+        self.hand = list[Cards.Card]
+        self.my_houses = list[Map.City]
 
     def setup_colonists(self,colonist_data):
         """Setup the player colonists according to the database
@@ -42,25 +44,29 @@ class Player:
         for i in range (len(colonist_data)):
             type = colonist_data[i][0]
             n_copies = colonist_data[i][2]
-            for y in range (0,n_copies-1):
+            for y in range (n_copies):
                 self.my_store_house.my_pieces.append(Pieces.Colonist(type,self.color))
     
     def setup_goods(self,goods_data):
-        print(goods_data)
         for i in range (len(goods_data)):
             type = goods_data[i][0]
             n_copies = goods_data[i][1]
-            for y in range (0,n_copies-1):
+            for y in range (n_copies):
                 self.my_store_house.my_pieces.append(Pieces.Resource(Pieces.ResourceType.RESOURCE_TYPES[type]))
 
     def setup_houses(self,houses_data):
-        pass
+        Player.MAX_HOUSES = houses_data[0][0]
 
     def setup_cards(self,cards_data):
-        pass
-
+        print(cards_data)
+        for i in range (len(cards_data)):
+            type = cards_data[i][0]
+            n_copies = cards_data[i][1]
+            for y in range (n_copies):
+                self.hand.append(Cards.Card(type))
+        
     def setup_sestertii(self,sestersii_data):
-        pass
+        self.money = sestersii_data[1]
 
     def play_card(self, card: Cards.Card):
         """ Play a card ( and her effect)
